6853ae4875a95482eb61bbcef50540e96e185466
juan123-4 <juandibarra09@gmail.com>
2024-11-03 15:25:29 +0100

    actualizacion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
  // Aquí tu código
+const boton = document.getElementById("agregar")
+
+
+boton.addEventListener("click",function(){
+    let dato = prompt("coloca tu nombre");
+    let punto =document.createElement("li");
+    const nuevo = document.createTextNode(dato);
+    punto.appendChild(nuevo);
+    const lista =document.getElementById("lista");
+    document.body.insertBefore(punto,lista);    
+    
+
+})
